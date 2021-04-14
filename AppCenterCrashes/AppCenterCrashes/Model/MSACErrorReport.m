a3f519c67f0eaed4a2562c04ecaddc5fdd1c033b
Anastasia Kubova <v-ankubo@microsoft.com>
2021-04-14 10:10:13 +0300

    Add serialize for ErrorReport

--- AppCenterCrashes/AppCenterCrashes/Model/MSACErrorReport.m
+++ AppCenterCrashes/AppCenterCrashes/Model/MSACErrorReport.m
@@ -4,6 +4,16 @@
 #import "MSACErrorReport.h"
 #import "MSACErrorReportPrivate.h"
 
+static NSString *const kMSACIncidentIdentifier = @"incidentIdentifier";
+static NSString *const kMSACReporterKey = @"reporterKey";
+static NSString *const kMSACSignal = @"signal";
+static NSString *const kMSACExceptionName = @"exceptionName";
+static NSString *const kMSACExceptionReason = @"exceptionReason";
+static NSString *const kMSACAppStartTime = @"appStartTime";
+static NSString *const kMSACAppErrorTime = @"appErrorTime";
+static NSString *const kMSACDevice = @"device";
+static NSString *const kMSACAppProcessIdentifier = @"appProcessIdentifier";
+
 @interface MSACErrorReport ()
 
 @property(nonatomic, copy) NSString *signal;
@@ -43,6 +53,42 @@
     result = YES;
 
   return result;
+}
+
+- (NSMutableDictionary *)serializeToDictionary {
+  NSMutableDictionary *dict = [NSMutableDictionary new];
+    if (self.incidentIdentifier) {
+      dict[kMSACIncidentIdentifier] = self.incidentIdentifier;
+    }
+    if (self.reporterKey) {
+      dict[kMSACReporterKey] = self.reporterKey;
+    }
+    if (self.signal) {
+      dict[kMSACSignal] = self.signal;
+    }
+    if (self.exceptionName) {
+      dict[kMSACExceptionName] = self.exceptionName;
+    }
+    if (self.exceptionReason) {
+      dict[kMSACExceptionReason] = self.exceptionReason;
+    }
+    if (self.appStartTime) {
+      dict[kMSACAppStartTime] = self.appStartTime;
+    }
+    if (self.appErrorTime) {
+      dict[kMSACAppErrorTime] = self.appErrorTime;
+    }
+    if (self.device) {
+      dict[kMSACDevice] = self.device;
+    }
+    if (self.appProcessIdentifier) {
+      dict[kMSACAppProcessIdentifier] = [NSString stringWithFormat:@"%lu",(unsigned long) self.appProcessIdentifier];
+    }
+    return dict;
+}
+
+- (NSString *)description {
+    return [NSString stringWithFormat:@"%@", [self serializeToDictionary]];
 }
 
 @end
