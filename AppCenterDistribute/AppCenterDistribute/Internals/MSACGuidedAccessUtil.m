bec6d2170137776d8ae6bb77a9fdd94e936fcdc9
Tommy Warner <tommy.warner@hypr.com>
2021-08-17 10:08:23 -0400

    Updated to check for osx

--- AppCenterDistribute/AppCenterDistribute/Internals/MSACGuidedAccessUtil.m
+++ AppCenterDistribute/AppCenterDistribute/Internals/MSACGuidedAccessUtil.m
@@ -1,14 +1,20 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#if !TARGET_OS_OSX
 #import <UIKit/UIKit.h>
+#endif
 
 #import "MSACGuidedAccessUtil.h"
 
 @implementation MSACGuidedAccessUtil
 
 + (BOOL)isGuidedAccessEnabled {
+#if !TARGET_OS_OSX
   return UIAccessibilityIsGuidedAccessEnabled();
+#else
+    return no;
+#endif
 }
 
 @end
